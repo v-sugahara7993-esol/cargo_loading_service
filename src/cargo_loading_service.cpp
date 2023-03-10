ea379dae9a27daa5a5417de46f0eed89d258f646
v-sugahara7993-esol <109567391+v-sugahara7993-esol@users.noreply.github.com>
2023-03-10 17:20:37 +0900

    fix: change to return failure immediately after the judgment
    Signed-off-by: v-sugahara7993-esol <v-sugahara7993@esol.co.jp>
    
    Co-authored-by: Yuma Nihei <yuma.nihei@tier4.jp>

--- src/cargo_loading_service.cpp
+++ src/cargo_loading_service.cpp
@@ -71,6 +71,12 @@ void CargoLoadingService::execCargoLoading(
   const ExecuteInParkingTask::Response::SharedPtr response)
 {
   // 設備ID取得
+  if (request->id < static_cast<uint8_t>(InfraIdLimit::MIN) ||
+      request->id > static_cast<uint8_t>(InfraIdLimit::MAX)) {
+      RCLCPP_WARN(this->get_logger(), "Invalid ID = %d", request->id);
+      response->state = ExecuteInParkingTask::Response::FAIL;
+      return;
+  }
   infra_id_ = request->id;
   service_result_ = ExecuteInParkingTask::Response::SUCCESS;
 
